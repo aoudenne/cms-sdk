24f01cd539c97279bd634f2a5e3e5ff759311e45
vyastyk <Volodymyr.Yatsyk@bazaarvoice.com>
2014-04-08 14:03:07 +0300

    Modified JsonHelper class for proper handling of JsonExceptions.

--- src/main/java/com/bazaarvoice/cms/client/JsonHelper.java
+++ src/main/java/com/bazaarvoice/cms/client/JsonHelper.java
@@ -1,5 +1,7 @@
 package com.bazaarvoice.cms.client;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.MappingJsonFactory;
 import org.codehaus.jackson.map.ObjectMapper;
 
@@ -9,20 +11,14 @@ import java.io.IOException;
 public class JsonHelper {
     private static final ObjectMapper JSON = new MappingJsonFactory().getCodec();
 
-    public static String toJson(Object obj){
-        try {
-            return JSON.writeValueAsString(obj);
-        } catch (IOException e) {
-            throw new AssertionError(e);
-        }
+    public static String toJson(Object obj)
+            throws IOException, JsonParseException, JsonMappingException {
+        return JSON.writeValueAsString(obj);
     }
 
-    public static <T> T fromJson(String string, Class<T> type) {
-        try {
-            return JSON.readValue(string, type);
-        } catch (IOException e) {
-            throw new AssertionError(e);  // Shouldn't get IO errors reading from a string
-        }
+    public static <T> T fromJson(String string, Class<T> type)
+            throws IOException, JsonParseException, JsonMappingException {
+        return JSON.readValue(string, type);
     }
 
     public static <T> T fromJson(File file, Class<T> type)
